ed6a277a0478b3e77d8b590010b5fac43ce641c3
Rainer Krug <Rainer@krugs.de>
2021-11-11 14:06:30 +0100

    Prepare move of add_strain_var() function to microxaox package

--- R/various_useful_functions.r
+++ R/various_useful_functions.r
@@ -1,15 +1,40 @@
-add_strain_var <- function(x,
-                           CB_var_gmax = 0, CB_var_h = 0,
-                           SB_var_gmax = 0, SB_var_h = 0,
-                           PB_var_gmax = 0, PB_var_h = 0) {
-  new_x <- x
-  new_x$CB$g_max_CB <- x$CB$g_max_CB * 2^(seq(-CB_var_gmax, CB_var_gmax, length = length(new_x$CB$g_max_CB)))
-  new_x$CB$h_SR_CB <- x$CB$h_SR_CB * 2^(seq(-CB_var_h, CB_var_h, length = length(new_x$CB$h_SR_CB)))
-  new_x$SB$g_max_SB <- x$SB$g_max_SB * 2^(seq(-SB_var_gmax, SB_var_gmax, length = length(new_x$SB$g_max_SB)))
-  new_x$SB$h_O_SB <- x$SB$h_O_SB * 2^(seq(-SB_var_h, SB_var_h, length = length(new_x$SB$h_O_SB)))
-  new_x$PB$g_max_PB <- x$PB$g_max_PB * 2^(seq(-PB_var_gmax, PB_var_gmax, length = length(new_x$PB$g_max_PB)))
-  new_x$PB$h_O_PB <- x$PB$h_O_PB * 2^(seq(-PB_var_h, PB_var_h, length = length(new_x$PB$h_O_PB)))
-  new_x
+add_strain_var <- function(
+  x,
+  CB_var_gmax = 0, 
+  CB_var_h = 0,
+  SB_var_gmax = 0, 
+  SB_var_h = 0,
+  PB_var_gmax = 0, 
+  PB_var_h = 0,
+  method = "even"
+){
+
+  even <- function(
+    x,
+    var
+  ){
+    x * 2^(seq(-var, var, length = length(x)))
+  }
+  
+  switch (method,
+    "even" = {
+      x$CB$g_max_CB <- even(x$CB$g_max_CB, CB_var_gmax)
+      x$CB$h_SR_CB  <- even(x$CB$h_SR_CB, CB_var_h    )
+      x$SB$g_max_SB <- even(x$SB$g_max_SB, SB_var_gmax)
+      x$SB$h_O_SB   <- even(x$SB$h_O_SB ,SB_var_h     )
+      x$PB$g_max_PB <- even(x$PB$g_max_PB, PB_var_gmax)
+      x$PB$h_O_PB   <- even(x$PB$h_O_PB, PB_var_h     )
+    },
+    stop("Method '", method, "' not implemented!")
+  )
+  
+  # new_x$CB$g_max_CB <- x$CB$g_max_CB * 2^(seq(-CB_var_gmax, CB_var_gmax, length = length(new_x$CB$g_max_CB)))
+  # new_x$CB$h_SR_CB  <- x$CB$h_SR_CB  * 2^(seq(-CB_var_h,    CB_var_h,    length = length(new_x$CB$h_SR_CB) ))
+  # new_x$SB$g_max_SB <- x$SB$g_max_SB * 2^(seq(-SB_var_gmax, SB_var_gmax, length = length(new_x$SB$g_max_SB)))
+  # new_x$SB$h_O_SB   <- x$SB$h_O_SB   * 2^(seq(-SB_var_h,    SB_var_h,    length = length(new_x$SB$h_O_SB)  ))
+  # new_x$PB$g_max_PB <- x$PB$g_max_PB * 2^(seq(-PB_var_gmax, PB_var_gmax, length = length(new_x$PB$g_max_PB)))
+  # new_x$PB$h_O_PB   <- x$PB$h_O_PB   * 2^(seq(-PB_var_h,    PB_var_h,    length = length(new_x$PB$h_O_PB)  ))
+  return(x)
 }
 
 
@@ -450,13 +475,15 @@ create_diversity <- function(
       SB_var_gmax_s, SB_var_h_s,
       PB_var_gmax_s, PB_var_h_s
     ) %>%
-    mutate(pars = list(add_strain_var(default_9strain,
+    mutate(pars = list(add_strain_var(
+      x = default_9strain,
       CB_var_gmax = CB_var_gmax_s,
       CB_var_h = CB_var_h_s,
       SB_var_gmax = SB_var_gmax_s,
       SB_var_h = SB_var_h_s,
       PB_var_gmax = PB_var_gmax_s,
-      PB_var_h = PB_var_h_s
+      PB_var_h = PB_var_h_s,
+      method = "even"
     )))
 
   var_expt
@@ -502,13 +529,15 @@ create_diversity_factorial <- function(
       SB_var_gmax_s, SB_var_h_s,
       PB_var_gmax_s, PB_var_h_s
     ) %>%
-    mutate(pars = list(add_strain_var(default_9strain,
+    mutate(pars = list(add_strain_var(
+      x = default_9strain,
       CB_var_gmax = CB_var_gmax_s,
       CB_var_h = CB_var_h_s,
       SB_var_gmax = SB_var_gmax_s,
       SB_var_h = SB_var_h_s,
       PB_var_gmax = PB_var_gmax_s,
-      PB_var_h = PB_var_h_s
+      PB_var_h = PB_var_h_s,
+      method = "even"
     )))
 
   var_expt
