7fbf440ebf7154a5f8d5e59cfd4f06253cf06f2e
Owen Petchey <owen.petchey@ieu.uzh.ch>
2021-11-26 15:22:02 +0100

    add system time

--- experiments/0_ss_finding/run_experiment.R
+++ experiments/0_ss_finding/run_experiment.R
@@ -18,10 +18,12 @@ source(here("experiments/0_ss_finding/setup_experiment.R"))
 ## Get SS
 #var_expt <- var_expt[1:8,]
 num_cores = 12
+system.time({
 expt_res <- run_temporal_ssfind_experiment(parameter,
                                            var_expt,
                                            total_initial_abundances = total_initial_abundances,
                                            cores = num_cores)
+})
 saveRDS(expt_res, here("experiments/0_ss_finding/data/", paste0(num_strains, "_strain_SS_data.RDS")))
 
 
@@ -30,11 +32,13 @@ num_cores = 12
 expt_res <- readRDS(here("experiments/0_ss_finding/data/", paste0(num_strains, "_strain_SS_data.RDS")))
 cluster1 <- multidplyr::new_cluster(num_cores)
 multidplyr::cluster_library(cluster1, c("microxanox", "dplyr"))
+system.time({
 stab_data <-  expt_res %>%
   multidplyr::partition(cluster1) %>%
   mutate(stability_measures = list(get_stability_measures_new(ssfind_result))) %>%
   collect() %>%
   unnest(cols = c(stability_measures)) 
+})
 saveRDS(stab_data, here("experiments/0_ss_finding/data/", paste0(num_strains, "_strain_stab_data.RDS")))
 
 
