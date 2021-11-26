4613b87b1985654b27591fa0ce1e62d9d07be296
Rainer M. Krug <Rainer@krugs.de>
2021-11-26 10:27:20 +0100

    Error isolation and solution steps

--- experiments/0_ss_finding/run_experiment.R
+++ experiments/0_ss_finding/run_experiment.R
@@ -17,18 +17,20 @@ source(here("experiments/0_ss_finding/setup_experiment.R"))
 
 ## Get SS
 var_expt <- var_expt[1:2,]
+no_cores = 1
 expt_res <- run_temporal_ssfind_experiment(parameter,
                                            var_expt,
                                            total_initial_abundances = total_initial_abundances,
-                                           cores = 1)
-saveRDS(expt_res, here("experiments/0_ss_finding/data/", paste0(num_strains, "_strain_SS_data.RDS")))
+                                           cores = no_cores)
+saveRDS(expt_res, here("experiments/0_ss_finding/data/", paste0(no_cores, "c_rowwise_", num_strains, "_strain_SS_data.RDS")))
 
 
 ## Get stability
-expt_res <- readRDS(here("experiments/0_ss_finding/data/", paste0(num_strains, "_strain_SS_data.RDS")))
-cluster1 <- multidplyr::new_cluster(2)
+no_cores = 6
+expt_res <- readRDS(here("experiments/0_ss_finding/data/", paste0(no_cores, "c_rowwise_", num_strains, "_strain_SS_data.RDS")))
+cluster1 <- multidplyr::new_cluster(1)
 multidplyr::cluster_library(cluster1, c("microxanox", "dplyr"))
-stab_data <-  expt_res_single %>%
+stab_data <-  expt_res %>%
   multidplyr::partition(cluster1) %>%
   mutate(stability_measures = list(get_stability_measures_new(ssfind_result))) %>%
   collect() %>%
