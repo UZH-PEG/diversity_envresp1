ae9fdc85f61267781462d00f2c92296f5cf48fa5
Owen Petchey <owen.petchey@ieu.uzh.ch>
2022-02-27 09:16:52 +0100

    work on supplement

--- experiments/0_ss_finding/temporal_method/data_processing.R
+++ experiments/0_ss_finding/temporal_method/data_processing.R
@@ -3,9 +3,9 @@
 library(here)
 library(tidyverse)
 
+## Process the stability data to a smaller file
 
-
-## Process the stability data to a smaller file ----
+##  event def2 ----
 
 read_plus <- function(flnm) {
   print(flnm)
@@ -13,10 +13,10 @@ read_plus <- function(flnm) {
     mutate(filename = flnm)
 }
 
-tbl1 <- list.files(path = here::here("data/0_ss_finding/temporal_method/"),
+tbl1 <- list.files(path = here::here("data/0_ss_finding/temporal_method"),
                    pattern = "stab_data", 
                    full.names = TRUE,
-                   recursive = TRUE) %>% 
+                   recursive = FALSE) %>% 
   map_df(~read_plus(.))
 
 keep_tbl1 <- tbl1
@@ -115,6 +115,121 @@ all_stab_results <- all_stab_results %>%
                                                                 "CB-SB-PB")))
 
 saveRDS(all_stab_results, here::here("data/0_ss_finding/temporal_method/processed_data/stab_data_temporal_method.RDS"))
+
+
+
+
+## Event def1 ----
+
+read_plus <- function(flnm) {
+  print(flnm)
+  readRDS(flnm) %>% 
+    mutate(filename = flnm)
+}
+
+tbl1 <- list.files(path = here::here("data/0_ss_finding/temporal_method/event_def1"),
+                   pattern = "stab_data", 
+                   full.names = TRUE,
+                   recursive = FALSE) %>% 
+  map_df(~read_plus(.))
+
+keep_tbl1 <- tbl1
+
+all_stab <- tbl1 %>%
+  select(-data, -pars, -parameter, -total_initial_abundances, -ssfind_pars, -ssfind_result) %>%
+  separate(filename , sep = "_", into = c(paste0("junk", 1:8), "waittime", paste0("junk", 9:11))) %>%
+  select(-starts_with("junk")) %>%
+  mutate(waittime = parse_number(waittime))
+
+
+
+CB_vars <- unique(all_stab$CB_var_gmax_s)
+SB_vars <- unique(all_stab$SB_var_gmax_s)
+PB_vars <- unique(all_stab$PB_var_gmax_s)
+
+CB_all_stab <- all_stab %>%
+  filter(SB_var_gmax_s == 0,
+         PB_var_gmax_s == 0) %>%
+  ungroup() %>%
+  mutate(var_treat = "CB",
+         var_gmax = CB_var_gmax_s,
+         stand_var = var_gmax / max(var_gmax))
+SB_all_stab <- all_stab %>%
+  filter(CB_var_gmax_s == 0,
+         PB_var_gmax_s == 0)  %>%
+  ungroup() %>%
+  mutate(var_treat = "SB",
+         var_gmax = SB_var_gmax_s,
+         stand_var = var_gmax / max(var_gmax))
+PB_all_stab <- all_stab %>%
+  filter(CB_var_gmax_s == 0,
+         SB_var_gmax_s == 0) %>%
+  ungroup()  %>%
+  mutate(var_treat = "PB",
+         var_gmax = PB_var_gmax_s,
+         stand_var = var_gmax / max(var_gmax))
+
+for_join <- tibble(CB_var_gmax_s = sort(CB_vars),
+                   SB_var_gmax_s = sort(SB_vars))
+CBSB_all_stab <- all_stab %>%
+  right_join(for_join)  %>%
+  ungroup() %>%
+  mutate(var_treat = "CB-SB",
+         var_gmax = CB_var_gmax_s,
+         stand_var = var_gmax / max(var_gmax)) %>%
+  filter(PB_var_gmax_s == 0)
+
+for_join <- tibble(CB_var_gmax_s = sort(CB_vars),
+                   PB_var_gmax_s = sort(PB_vars))
+CBPB_all_stab <- all_stab %>%
+  right_join(for_join)  %>%
+  ungroup() %>%
+  mutate(var_treat = "CB-PB",
+         var_gmax = CB_var_gmax_s,
+         stand_var = var_gmax / max(var_gmax)) %>%
+  filter(SB_var_gmax_s == 0)
+
+for_join <- tibble(SB_var_gmax_s = sort(SB_vars),
+                   PB_var_gmax_s = sort(PB_vars))
+SBPB_all_stab <- all_stab %>%
+  right_join(for_join)  %>%
+  ungroup() %>%
+  mutate(var_treat = "SB-PB",
+         var_gmax = SB_var_gmax_s,
+         stand_var = var_gmax / max(var_gmax)) %>%
+  filter(CB_var_gmax_s == 0)
+
+
+
+for_join <- tibble(CB_var_gmax_s = sort(CB_vars),
+                   SB_var_gmax_s = sort(SB_vars),
+                   PB_var_gmax_s = sort(PB_vars))
+CBSBPB_all_stab <- all_stab %>%
+  right_join(for_join)  %>%
+  ungroup() %>%
+  mutate(var_treat = "CB-SB-PB",
+         var_gmax = CB_var_gmax_s,
+         stand_var = var_gmax / max(var_gmax))
+
+all_stab_results <- CB_all_stab %>%
+  bind_rows(SB_all_stab) %>%
+  bind_rows(PB_all_stab) %>%
+  bind_rows(CBSB_all_stab) %>%
+  bind_rows(CBPB_all_stab) %>%
+  bind_rows(SBPB_all_stab) %>%
+  bind_rows(CBSBPB_all_stab)
+
+all_stab_results <- all_stab_results %>%
+  mutate(var_treat = forcats::fct_relevel(var_treat, levels = c("CB",
+                                                                "SB",
+                                                                "PB",
+                                                                "CB-SB",
+                                                                "CB-PB",
+                                                                "SB-PB",
+                                                                "CB-SB-PB")))
+
+saveRDS(all_stab_results, here::here("data/0_ss_finding/temporal_method/event_def1/processed_data/stab_data_temporal_method.RDS"))
+
 
 
 
