c88249586e294dac171c9cdfadcec3b2ed860e0a
Owen Petchey <owen.petchey@ieu.uzh.ch>
2022-06-09 13:21:34 +0200

    update supplement

--- R/ms_figure_functions.R
+++ R/ms_figure_functions.R
@@ -892,6 +892,250 @@ fig_state_vs_o2diff_sidebyside_alternative <- function(ss_result){
   return(patchwork)
 }
 
+fig_state_vs_o2diff_sidebyside_alternative_repmeth <- function(ss_result){
+  
+  is_repl <- "ss_by_a_N_result" %in% names(ss_result)
+  
+  if (is_repl) {
+    ss_result$ssfind_result <- ss_result$ss_res
+  }
+  
+  temp1 <- ss_result %>%
+    mutate(ssfind_result = list(get_total_bio(ssfind_result)))
+  
+  if (is_repl){
+    temp1 <- microxanox:::get_stability_measures.replication_ssfind_result(
+      temp1$ssfind_result[[1]],
+      threshold_diff_log10scale = 3
+    )
+  } else {
+    temp1 <- microxanox:::get_stability_measures.temporal_ssfind_result(
+      temp1$ssfind_result[[1]],
+      threshold_diff_log10scale = 3
+    )
+  }
+  
+  stab_measures <- temp1 %>% 
+    filter(Species == "SB_tot") %>%
+    select(down=hyst_min_log, up=hyst_max_log) %>%
+    pivot_longer(names_to = "direction", values_to = "aO_flip", 1:2) %>%
+    mutate(y_start = c(2.5, 7.5),
+           y_end = c(7.5, 2.5))
+  
+  rm(temp1)
+  
+  if (is_repl) {
+    gathcols <- 1:(31)
+    ss_result$ssfind_result[[1]] <- ss_result$ssfind_result[[1]] %>%
+      mutate(a_O = log10(a_O),
+             direction = ifelse(initial_N_CB == 1, "up", "down"))
+  } else {
+    gathcols <- 2:(ncol(ss_result$ssfind_result[[1]]) - 2)
+  }
+  
+  temp <- ss_result$ssfind_result[[1]] %>%
+    mutate(a = 10^a_O) %>%
+    gather(species, quantity, gathcols) %>%
+    mutate(
+      var_type = ifelse(grepl("B_", species), "Organism", "Substrate"),
+      functional_group = case_when(
+        str_detect(species, "CB_") ~ "CB",
+        str_detect(species, "SB_") ~ "SB",
+        str_detect(species, "PB_") ~ "PB"),
+      log10_quantity = log10(quantity + 1),
+      species = factor(species, levels = unique(species)),
+      functional_group2 = case_when(
+        functional_group == "CB" ~ "Cyanobacteria",
+        functional_group == "SB" ~ "Sulfate-reducing\nbacteria",
+        functional_group == "PB" ~ "Phototrophic\nsulfur bacteria"),
+      substrate2 = case_when(
+        var_type == "Substrate" & species == "SR" ~ "Sulfide\nconcentration",
+        var_type == "Substrate" & species == "O" ~ "Oxygen\nconcentration")
+    )
+  
+  num_strains <- temp %>%
+    group_by(functional_group) %>%
+    summarise(num = length(unique(species))) %>%
+    na.omit()
+  
+  num_CB_strains <- num_strains$num[num_strains$functional_group == "CB"]
+  num_SB_strains <- num_strains$num[num_strains$functional_group == "SB"]
+  num_PB_strains <- num_strains$num[num_strains$functional_group == "PB"]
+  
+  
+  line_width <- 0.2
+  point_size <- 0.6
+  arrow_lwd <- 1
+  
+  p1CB <-
+    temp %>%
+    dplyr::filter(functional_group == "CB")  %>%
+    ggplot(aes(x = log10(a), y = log10_quantity, col = species)) +
+    geom_segment(data = stab_measures,
+                 aes(x = aO_flip, xend = aO_flip,
+                     y = y_start, yend = y_end),
+                 col = "#bbbbbbff", lwd = arrow_lwd,
+                 arrow = arrow(length=unit(0.30,"cm"),
+                               ends="first", type = "closed")) +
+    geom_path(lwd = line_width) +
+    geom_point(size = point_size) +
+    scale_colour_manual(values = colfunc_CB(num_CB_strains)) +
+    guides(colour = guide_legend(ncol = 3)) +
+    labs(tag="a")+
+    theme_bw() +
+    theme(legend.position="none",
+          axis.title = element_blank(),
+          axis.ticks.x = element_blank(),
+          axis.text.x = element_blank(),
+          strip.background = element_rect(fill = "white"),
+          strip.background.x = element_blank(),
+          strip.text.x = element_blank(),
+          strip.text.y = element_text(size=12, margin = margin(l=11, r=11)),
+          plot.margin = margin(t=-1, b=-1)) +
+    facet_grid(functional_group2 ~ direction)
+  
+  p2SB <- temp %>%
+    dplyr::filter(functional_group == "SB")  %>%
+    ggplot(aes(x = log10(a), y = log10_quantity, col = species)) +
+    geom_segment(data = stab_measures,
+                 aes(x = aO_flip, xend = aO_flip,
+                     y = c(7.5, 2), yend = c(2, 7.5)),
+                 col = "#bbbbbbff", lwd = arrow_lwd,
+                 arrow = arrow(length=unit(0.30,"cm"),
+                               ends="first", type = "closed")) +
+    geom_path(lwd = line_width) +
+    geom_point(size = point_size) +
+    scale_colour_manual(values = colfunc_SB(num_SB_strains)) +
+    guides(colour = guide_legend(ncol = 3)) +
+    labs(tag="b")+
+    theme_bw() +
+    theme(legend.position="none",
+          axis.title = element_blank(),
+          axis.ticks.x = element_blank(),
+          axis.text.x = element_blank(),
+          strip.background = element_rect(fill = "white"),
+          strip.background.x = element_blank(),
+          strip.text.x = element_blank(),
+          strip.text.y = element_text(size=12),
+          plot.margin = margin(t=-1, b=-1)) +
+    facet_grid(functional_group2 ~ direction)
+  
+  
+  p3PB <- temp %>%
+    dplyr::filter(functional_group == "PB")  %>%
+    ggplot(aes(x = log10(a), y = log10_quantity, col = species)) +
+    geom_segment(data = stab_measures,
+                 aes(x = aO_flip, xend = aO_flip,
+                     y = c(7, 2), yend = c(2, 7)),
+                 col = "#bbbbbbff", lwd = arrow_lwd,
+                 arrow = arrow(length=unit(0.30,"cm"),
+                               ends="first", type = "closed")) +
+    geom_path(lwd = line_width) +
+    geom_point(size = point_size) +
+    scale_colour_manual(values = colfunc_PB(num_PB_strains)) +
+    guides(colour = guide_legend(ncol = 3)) +
+    labs(tag="c") +
+    theme_bw() +
+    theme(legend.position="none",
+          axis.ticks.x = element_blank(),
+          axis.text.x = element_blank(),
+          axis.title = element_blank(),
+          strip.background = element_rect(fill = "white"),
+          strip.background.x = element_blank(),
+          strip.text.x = element_blank(),
+          strip.text.y = element_text(size=12),
+          plot.margin = margin(t=-1, b=-1)) +
+    facet_grid(functional_group2 ~ direction)
+  
+  
+  
+  p4O <- temp %>%
+    dplyr::filter(var_type == "Substrate", species == "O") %>%
+    ggplot(aes(x = log10(a), y = log10_quantity, col = species)) +
+    geom_segment(data = stab_measures,
+                 aes(x = aO_flip, xend = aO_flip,
+                     y = c(0.5, 2), yend = c(2, 0.5)),
+                 col = "#bbbbbbff", lwd = arrow_lwd,
+                 arrow = arrow(length=unit(0.30,"cm"),
+                               ends="first", type = "closed")) +
+    geom_path(lwd = line_width) +
+    geom_point(size = point_size) +
+    labs(tag="d")+
+    theme_bw() +
+    theme(legend.position="none",
+          axis.title = element_blank(),
+          axis.ticks.x = element_blank(),
+          axis.text.x = element_blank(),
+          strip.background = element_rect(fill = "white"),
+          strip.background.x = element_blank(),
+          strip.text.x = element_blank(),
+          strip.text.y = element_text(size=12),
+          plot.margin = margin(t=-1, b=-1)) +
+    facet_grid(substrate2 ~ direction)
+  
+  
+  
+  p5SR <- temp %>%
+    dplyr::filter(var_type == "Substrate", species == "SR") %>%
+    ggplot(aes(x = log10(a), y = log10_quantity, col = species)) +
+    geom_segment(data = stab_measures,
+                 aes(x = aO_flip, xend = aO_flip,
+                     y = c(2.5, 1.5), yend = c(1.5, 2.5)),
+                 col = "#bbbbbbff", lwd = arrow_lwd,
+                 arrow = arrow(length=unit(0.30,"cm"),
+                               ends="first", type = "closed")) +
+    geom_path(lwd = line_width, col=6) +
+    geom_point(size = point_size, col=6) +
+    labs(tag="e", x=expression('log'[10]*"(oxygen diffusivity) (h"^{-1}*")"))+
+    theme_bw() +
+    theme(legend.position="none",
+          axis.title.y = element_blank(),
+          axis.title.x = element_text(size=13),
+          strip.background = element_rect(fill = "white"),
+          strip.background.x = element_blank(),
+          strip.text.x = element_blank(),
+          strip.text.y = element_text(size=12),
+          plot.margin = margin(t=-1, b=-1)) +
+    facet_grid(substrate2 ~ direction)
+  
+  
+  
+  ylab1 <- ggplot(data.frame(x = 1, y = 4.1)) +
+    geom_text(aes(x, y),label= expression('log'[10]*"(density) (cells L"^{-1}*")"),
+              angle = 90, size=4.5) + 
+    theme_void() +
+    coord_cartesian(clip = "off") +
+    theme(plot.margin = margin(t=-1, b=-1, l=-1))
+  
+  
+  ylab2 <- ggplot(data.frame(x = 1, y = 4.1)) +
+    geom_text(aes(x, y),label= expression('log'[10]*"(concentration) ("*mu*"M)"),
+              angle = 90, size=4.5) + 
+    theme_void() +
+    coord_cartesian(clip = "off") +
+    theme(plot.margin = margin(t=-1, b=-1, l=-1))
+  
+  
+  direction.facet <- ggplot(data.frame(x = 1, y = 1,direction = c("High initial CB density","Low initial CB density"))) +
+    geom_text(aes(x, y, label = direction), size=4.5) +
+    facet_wrap(~direction)+
+    theme_bw()+
+    theme(strip.background = element_blank(),
+          strip.text = element_blank(),
+          panel.grid = element_blank(),
+          axis.title = element_blank(),
+          axis.ticks = element_blank(),
+          axis.text = element_blank(),
+          plot.margin = margin(t=-2, b=-1))
+  
+  
+  patchwork <- plot_spacer() + direction.facet +
+    ylab1 + (p1CB/p2SB/p3PB) +
+    ylab2 + (p4O/p5SR) +
+    plot_layout(ncol = 2, heights = c(1,15,10), widths = c(1,30))
+  
+  return(patchwork)
+}
 
 
 
